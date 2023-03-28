75248eb600f22be7dac5f3c2c2edf5e0bdd83b95
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-03-28 16:45:14 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Fares.Abdelkader All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares Abdelkader
+// Created on: Sep 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
